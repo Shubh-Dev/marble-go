9c509ed616c3d5a1e8eb5b3430e03e2bdee42e11
Shubh <shubh.scb@gmail.com>
2023-04-10 14:37:46 +0530

    feat: marble package added

--- app.go
+++ app.go
@@ -1,23 +1,23 @@
 package main
-
 import (
-	"github.com/gofiber/fiber"
-	// "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/Marbleture/api/marble"
 )
 
-
-func helloMarbel(c *fiber.Ctx) {
-	c.Send("Hello, Marbel")
-
+func setUpRoutes(app *fiber.App) {
+	app.Get("/marbles", marble.getMarbles)
+	app.Get("/marbles/:marbleId", marble.getMarble)
+	app.Post("/marbles", marble.newMarble)
+	app.Delete("/marbles/:marbleId", marble.deleteMarble)
 }
-
-
 
 func main() {
-	app :=fiber.New()
-
-	app.Get("/", helloMarbel)
-	
-	app.Listen(3000)
-
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Listen(":3000")
 }
+
+
+// Path: app_test.go
---
+++ marble/marble.go
@@ -0,0 +1,24 @@
+package marble
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+
+)
+
+func getMarbles(c *fiber.Ctx) error {
+	return c.SendString("Get all marbles")
+}
+
+func getMarble(c *fiber.Ctx) error {
+	return c.SendString("Get a single marble")
+}
+
+func newMarble(c *fiber.Ctx) error {
+	return c.SendString("create New marble")
+}	
+
+
+func deleteMarble(c *fiber.Ctx) error {
+	return c.SendString("Delete a single marble")
+}
